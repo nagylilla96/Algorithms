b16bef96bf87ace9bc23c4d1c440e314f38c0eb5
Nagy Lilla <n.lilla96@gmail.com>
2019-10-23 23:08:39 +0300

    Added graph implementation for three representations: adjacency list, adjacency matrix and edge list

---
+++ Graph/Header.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class GraphNode;
+
+typedef vector<GraphNode*> GRAPH;
+
+class GraphNode
+{
+public:
+	int value;
+	vector<GraphNode*> nodes;
+
+	GraphNode()
+	{
+		value = 0;
+	}
+
+	GraphNode(int val)
+	{
+		value = val;
+	}
+
+	void printAdjacencyList(vector<vector<int>> adjList);
+
+	vector<vector<int>> readAdjacencyList();
+
+	GRAPH graphFromAdjList(vector<vector<int>> adjList);
+
+	void printMatrix(int** matrix, int size);
+
+	int** readMatrix(int* size);
+
+	GRAPH graphFromMatrix(int** matrix, int size);
+
+	void printEdges(vector<pair<int, int>> edges);
+
+	vector<pair<int, int>> readEdges();
+
+	GRAPH graphFromEdges(vector<pair<int, int>> edges);
+
+	void printGraph(GRAPH graph);
+};
--- Graph/Source.cpp
+++ Graph/Source.cpp
@@ -1,102 +1,214 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
+#include "Header.h"
 
-using namespace std;
-
-class GraphNode
+void GraphNode::printAdjacencyList(vector<vector<int>> adjList)
 {
-public:
-	int value;
-	vector<GraphNode*> nodes;
-
-	GraphNode(int val)
+	for (auto line : adjList)
 	{
-		value = val;
+		for (auto num : line)
+		{
+			cout << num << " ";
+		}
+		cout << endl;
+	}
+}
+
+vector<vector<int>> GraphNode::readAdjacencyList()
+{
+	GraphNode* start = NULL;
+	vector<vector<int>> adjList;
+	ifstream file("list.txt", ios::in);
+	string str;
+
+	if (!file.good()) return adjList;
+
+	while (getline(file, str))
+	{
+		istringstream ss(str);
+		vector<int> temp;
+		int num, i = 0;
+		while (ss >> num)
+		{
+			temp.push_back(num);
+			i++;
+		}
+		adjList.push_back(temp);
 	}
 
-	static void printAdjacencyList(vector<vector<int>> adjList)
+	return adjList;
+}
+
+GRAPH GraphNode::graphFromAdjList(vector<vector<int>> adjList)
+{
+	GRAPH graph;
+
+	for (unsigned int i = 0; i < adjList.size(); i++)
 	{
-		for (auto line : adjList)
+		graph.push_back(new GraphNode(i));
+		for (auto el : adjList[i])
 		{
-			for (auto num : line)
-			{
-				cout << num << " ";
-			}
-			cout << endl;
+			graph[i]->nodes.push_back(new GraphNode(el));
 		}
 	}
 
-	static GraphNode* readAdjacencyList()
+	return graph;
+}
+
+void GraphNode::printMatrix(int** matrix, int size)
+{
+	for (int i = 0; i < size; i++)
 	{
-		GraphNode* start = NULL;
-		vector<vector<int>> adjList;
-		ifstream file("list.txt", ios::in);
-		int nodes = 0;
-		string str;
-
-		if (!file.good()) return NULL;
-
-		while (getline(file, str))
+		for (int j = 0; j < size; j++)
 		{
-			istringstream ss(str);
-			vector<int> temp;
-			int num, i = 0;
-			while (ss >> num)
-			{
-				temp.push_back(num);
-				i++;
-			}
-			adjList.push_back(temp);
+			cout << matrix[i][j] << " ";
 		}
+		cout << endl;
+	}
+}
 
-		printAdjacencyList(adjList);
+int** GraphNode::readMatrix(int* size)
+{
+	int** matrix;
+
+	if (size == NULL) return NULL;
+
+	*size = 0;
+
+	ifstream file("matrix.txt", ios::in);
+
+	if (!file.good()) return NULL;
+
+	file >> *size;
+	matrix = (int**)malloc(sizeof(int*) * *size);
+	if (matrix == NULL) return NULL;
+
+	for (int i = 0; i < *size; i++)
+	{
+		matrix[i] = (int*)malloc(sizeof(int) * *size);
+		if (matrix[i] == NULL) return NULL;
+
+		for (int j = 0; j < *size; j++)
+		{
+			file >> matrix[i][j];
+		}
 	}
 
-	static void printMatrix(int** matrix, int size)
+	return matrix;
+}
+
+GRAPH GraphNode::graphFromMatrix(int** matrix, int size)
+{
+	GRAPH graph;
+
+	for (int i = 0; i < size; i++)
 	{
-		for (int i = 0; i < size; i++)
+		graph.push_back(new GraphNode(i));
+		for (int j = 0; j < size; j++)
 		{
-			for (int j = 0; j < size; j++)
+			if (matrix[i][j] == 1)
 			{
-				cout << matrix[i][j] << " ";
+				graph[i]->nodes.push_back(new GraphNode(j));
 			}
-			cout << endl;
+
 		}
 	}
 
-	static GraphNode* readMatrix()
+	return graph;
+}
+
+void GraphNode::printEdges(vector<pair<int, int>> edges)
+{
+	for (auto e : edges)
 	{
-		int** matrix;
-		int size = 0;
-
-		ifstream file("matrix.txt", ios::in);
-
-		if (!file.good()) return NULL;
-
-		file >> size;
-		matrix = (int**)malloc(sizeof(int*) * size);
-		if (matrix == NULL) return NULL;
-
-		for (int i = 0; i < size; i++)
-		{
-			matrix[i] = (int*)malloc(sizeof(int) * size);
-			if (matrix[i] == NULL) return NULL;
-
-			for (int j = 0; j < size; j++)
-			{
-				file >> matrix[i][j];
-			}
-		}
-
-		printMatrix(matrix, size);
+		cout << e.first << " " << e.second << endl;
 	}
-};
+}
+
+vector<pair<int, int>> GraphNode::readEdges()
+{
+	vector<pair<int, int>> edges;
+	ifstream file("edges.txt", ios::in);
+	int a, b;
+
+	if (!file.good()) return edges;
+
+	while (file >> a >> b)
+	{
+		edges.push_back(make_pair(a, b));
+	}
+
+	return edges;
+}
+
+GRAPH GraphNode::graphFromEdges(vector<pair<int, int>> edges)
+{
+	GRAPH graph;
+
+	for (auto p : edges)
+	{
+		int i = p.first;
+		GraphNode* node;
+		try
+		{
+			node = graph.at(i);
+		}
+		catch (exception& e)
+		{
+			node = new GraphNode(i);
+			graph.push_back(node);
+		}
+		node->nodes.push_back(new GraphNode(p.second));
+	}
+
+	return graph;
+}
+
+void GraphNode::printGraph(GRAPH graph)
+{
+	for (auto node : graph)
+	{
+		cout << node->value << ": ";
+		for (auto c : node->nodes)
+		{
+			cout << c->value << " ";
+		}
+		cout << endl;
+	}
+}
+
+void testAdjacencyList(GraphNode graph)
+{
+	vector<vector<int>> adjList = graph.readAdjacencyList();
+	GRAPH adjGraph = graph.graphFromAdjList(adjList);
+	cout << "Graph from adjacency list: " << endl;
+	graph.printGraph(adjGraph);
+	cout << endl;
+}
+
+void testMatrix(GraphNode graph)
+{
+	int size = 0;
+	int** matrix = graph.readMatrix(&size);
+	GRAPH matrixGraph = graph.graphFromMatrix(matrix, size);
+	cout << "Graph from matrix: " << endl;
+	graph.printGraph(matrixGraph);
+	cout << endl;
+}
+
+void testEdges(GraphNode graph)
+{
+	vector<pair<int, int>> edges = graph.readEdges();
+	GRAPH edgeGraph = graph.graphFromEdges(edges);
+	cout << "Graph from edges: " << endl;
+	graph.printGraph(edgeGraph);
+}
 
 void test()
 {
-	GraphNode::readMatrix();
+	GraphNode graph;
+
+	testAdjacencyList(graph);
+	testMatrix(graph);
+	testEdges(graph);
 }
 
 int main()
